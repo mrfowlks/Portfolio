738bd2813c2355c0a502b0f862a74fe89ea17291
Morrison Fowlks <86127762+mrfowlks@users.noreply.github.com>
2022-01-13 21:00:31 -0600

    Delete Data Science directory

--- Data Science/Perfect Number Project/python/project.py
+++
@@ -1,9 +0,0 @@
-def functional_perf(number):
-    return number == sum(i for i in range(1, number) if number % i == 0)
-
-def imperative_perf(number):
-    sum = 0;
-    for i in range(1, number):
-        if number % i == 0:
-            sum += i
-    return sum == number
